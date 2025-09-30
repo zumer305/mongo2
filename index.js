a4e8caa871d931bb1493f51506b9f4245d9228ac
zumer305 <zumerniaz305@gmail.com>
2025-10-01 00:51:46 +0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // https://mongoosejs.com/ 
-
 // npm init -y(package.json)
 // npm i mongoose
 
+
+
+
+// --------------------------------------------------------------------------------
 const mongoose = require('mongoose');
 // let url="https://localhost:8080/user"; extra
-
-
-
 main()
 .then(()=>{
     console.log("success");
@@ -19,11 +19,48 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
   
 }
+// --------------------------------------------------------------------------------
 
 
-// Schema(table structure=columns)
+
+
+
+
+// set of rules 
 const userSchema=new mongoose.Schema({
     name:String,
     email:String,
     age:Number,
 });
+const user=mongoose.model("user",userSchema); //collection------ user(table)
+// show collections (cmd) 
+// capital=small
+// singular=plural
+const user1=new user({
+    name:"zumer",
+    age:23,
+    email:"zumer305@gmail.com",
+})
+// user1.save();
+// const user2=new user({
+//     name:"ali",
+//     age:24,
+//     email:"ali305@gmail.com",
+// })
+// user2.save()
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+user.insertMany([
+    {name:"wahab",age:12,email:"wahab@gmail.com"},
+    {name:"wahib",age:13,email:"wahib@gmail.com"},
+
+]).then((data)=>{
+    console.log(data);
+});
+// db.users.deleteMany({});
+user.find({age:{$gt:10}}).then((res)=> {console.log(res);}) //findOne
+.catch((err)=>{console.log (err);});
